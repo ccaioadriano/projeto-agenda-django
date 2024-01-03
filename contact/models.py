14c2fda0173e862e04714ab521c0fbfa19912a25
Caio Adriano <caiobhadriano@gmail.com>
2024-01-03 10:49:34 -0300

    [Feat]: Add model ´Category´

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.utils.timezone import now as current_date
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+            verbose_name = 'Category'
+            verbose_name_plural = 'Categories'
+            
+    def __str__(self) -> str:
+        return f'{self.name}'
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -10,6 +21,8 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
     created_at = models.DateTimeField(default=current_date)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    
     
     #toString
     def __str__(self) -> str:
